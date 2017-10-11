205a1787871f4465923b169fe5068bfe265e0308
emma <emmaruthoneill@gmail.com>
2017-10-11 14:18:23 -0700

    adding wineries

--- src/js/map-leaflet.js
+++ src/js/map-leaflet.js
@@ -37,6 +37,9 @@ var map = L.map("map-leaflet", {
 L.svg().addTo(map);
 
 // add tiles to the map
+
+//https://api.mapbox.com/styles/v1/emro/cj4g94j371v732rnptcghibsy/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZW1ybyIsImEiOiJjaXl2dXUzMGQwMDdsMzJuM2s1Nmx1M29yIn0._KtME1k8LIhloMyhMvvCDA
+//https://api.mapbox.com/styles/v1/emro/cj8lviggc6b302rqjyezdqc2m/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZW1ybyIsImEiOiJjaXl2dXUzMGQwMDdsMzJuM2s1Nmx1M29yIn0._KtME1k8LIhloMyhMvvCDA
 var mapLayer = L.tileLayer("https://api.mapbox.com/styles/v1/emro/cj8lviggc6b302rqjyezdqc2m/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZW1ybyIsImEiOiJjaXl2dXUzMGQwMDdsMzJuM2s1Nmx1M29yIn0._KtME1k8LIhloMyhMvvCDA",{attribution: '© <a href="https://www.mapbox.com/map-feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>'})
 mapLayer.addTo(map);
 
@@ -57,7 +60,7 @@ var evacuationIcon = new MapIcon({iconUrl: '../assets/graphics/evacuation_icon.p
 var hospitalsIcon = new MapIcon({iconUrl: '../assets/graphics/hospitalsEvacuated_icon.png?'});
 
 var grayIcon = new L.Icon({
-  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-grey.png',
+  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-violet.png',
   shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png?',
   iconSize: [25, 41],
   iconAnchor: [12, 41],
@@ -80,8 +83,18 @@ deaths_data.forEach(function(d){
   L.marker([d.Lat, d.Lng],{icon: grayIcon}).addTo(map).bindPopup(html_str);
 });
 
-// var myLayer = L.geoJSON().addTo(map);
-// myLayer.addData(geojsonFeature);
+var myStyle = {
+    "color": "#7D2E68",
+    "fill-opacity": 0.8,
+    "weight": 1,
+    "opacity": 1
+};
+
+var myLayer = L.geoJSON(wineriesGeoJson,{style:myStyle}).addTo(map);
+// console.log(wineriesGeoJson);
+// myLayer.addData(wineriesGeoJson);
+
+// var polygon = L.polygon(wineries_data["items"][0]["neighborhood_poly"]).addTo(map);
 
 var fireDataURL = "http://extras.sfgate.com/editorial/wildfires/noaa.csv?v=2";
 
