dad853b5b6afe2cc836b1954eabb937f1ef2d0c2
Lucio Villa <lucio310@gmail.com>
2017-10-11 14:31:25 -0700

    updated full width map js

--- src/js/map-leaflet-fullwidth.js
+++ src/js/map-leaflet-fullwidth.js
@@ -37,6 +37,9 @@ map.scrollWheelZoom.disable();
 L.svg().addTo(map);
 
 // add tiles to the map
+
+//https://api.mapbox.com/styles/v1/emro/cj4g94j371v732rnptcghibsy/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZW1ybyIsImEiOiJjaXl2dXUzMGQwMDdsMzJuM2s1Nmx1M29yIn0._KtME1k8LIhloMyhMvvCDA
+//https://api.mapbox.com/styles/v1/emro/cj8lviggc6b302rqjyezdqc2m/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZW1ybyIsImEiOiJjaXl2dXUzMGQwMDdsMzJuM2s1Nmx1M29yIn0._KtME1k8LIhloMyhMvvCDA
 var mapLayer = L.tileLayer("https://api.mapbox.com/styles/v1/emro/cj8lviggc6b302rqjyezdqc2m/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZW1ybyIsImEiOiJjaXl2dXUzMGQwMDdsMzJuM2s1Nmx1M29yIn0._KtME1k8LIhloMyhMvvCDA",{attribution: '© <a href="https://www.mapbox.com/map-feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>'})
 mapLayer.addTo(map);
 
@@ -57,7 +60,7 @@ var evacuationIcon = new MapIcon({iconUrl: '../assets/graphics/evacuation_icon.p
 var hospitalsIcon = new MapIcon({iconUrl: '../assets/graphics/hospitalsEvacuated_icon.png?'});
 
 var grayIcon = new L.Icon({
-  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-grey.png',
+  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-violet.png',
   shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png?',
   iconSize: [25, 41],
   iconAnchor: [12, 41],
@@ -80,8 +83,18 @@ deaths_data.forEach(function(d){
   L.marker([d.Lat, d.Lng],{icon: grayIcon}).addTo(map).bindPopup(html_str);
 });
 
-// var myLayer = L.geoJSON().addTo(map);
-// myLayer.addData(geojsonFeature);
+var myStyle = {
+    "color": "#7D2E68",
+    "fill-opacity": 0.8,
+    "weight": 1,
+    "opacity": 1
+};
+
+var myLayer = L.geoJSON(wineriesGeoJson,{style:myStyle}).addTo(map);
+// console.log(wineriesGeoJson);
+// myLayer.addData(wineriesGeoJson);
+
+// var polygon = L.polygon(wineries_data["items"][0]["neighborhood_poly"]).addTo(map);
 
 var fireDataURL = "http://extras.sfgate.com/editorial/wildfires/noaa.csv?v=2";
 
@@ -93,8 +106,8 @@ d3.csv(fireDataURL, function(fire_data){
 
   // creating Lat/Lon objects that d3 is expecting
   fire_data.forEach(function(d,idx) {
-  	d.LatLng = new L.LatLng(d.latitude,
-  							d.longitude);
+    d.LatLng = new L.LatLng(d.latitude,
+                d.longitude);
   });
 
   clearTimeout(map_timer);
@@ -149,13 +162,13 @@ var drawMap = function(fire_data) {
 
   // function that zooms and pans the data when the map zooms and pans
   function update() {
-  	circles.attr("transform",
-  	function(d) {
-  		return "translate("+
-  			map.latLngToLayerPoint(d.LatLng).x +","+
-  			map.latLngToLayerPoint(d.LatLng).y +")";
-  		}
-  	)
+    circles.attr("transform",
+    function(d) {
+      return "translate("+
+        map.latLngToLayerPoint(d.LatLng).x +","+
+        map.latLngToLayerPoint(d.LatLng).y +")";
+      }
+    )
   }
 
   map.on("viewreset", update);
@@ -198,13 +211,13 @@ var drawAir = function(air_data) {
 
   // function that zooms and pans the data when the map zooms and pans
   function update() {
-  	circles.attr("transform",
-  	function(d) {
-  		return "translate("+
-  			map.latLngToLayerPoint(d.LatLng).x +","+
-  			map.latLngToLayerPoint(d.LatLng).y +")";
-  		}
-  	)
+    circles.attr("transform",
+    function(d) {
+      return "translate("+
+        map.latLngToLayerPoint(d.LatLng).x +","+
+        map.latLngToLayerPoint(d.LatLng).y +")";
+      }
+    )
   }
 
   map.on("viewreset", update);
